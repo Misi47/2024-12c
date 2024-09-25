f7050bfa9ae9d80e5aaac96da849627144176836
Misi47 <Csire07@gmail.com>
2024-09-25 09:31:42 +0200

    a

--- 0925/Program.cs
+++ 0925/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace teszt
+namespace _0925
 {
     class Program
     {
         static void Main(string[] args)
         {
-
         }
     }
 }
---
+++ TobbElem/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+Projekt neve: TobbElem
+Írjunk olyan programot, amely addig kér
+be egész számokat a billentyűzetről, amíg azok összege meg nem haladja a 100 - at.A
+beolvasás végén írjuk ki azt, hogy a bekért számok közül hány volt páros, és hány volt
+páratlan.
+*/
+namespace TobbElem
+{
+    class TobbElemOsztaly
+    {
+        // Osztáy változók
+
+        private int szam = 0;
+        private bool isParos;
+
+        // Konstruktor
+        public TobbElemOsztaly()
+        {
+            this.szam = 0;
+            this.isParos = false;
+        }
+
+        // Metodus
+        public void GetSzam(int p)
+        {
+            this.szam = p;
+        }
+        public void SetSzamTipus()
+        {
+            this.isParos = szam % 2 == 0;
+        }
+        public bool outSzamTipus()
+        {
+            return this.isParos;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valasz = string.Empty;
+            int paros = 0,
+                szam = 0,
+            paratlan = 0;
+            
+             // Példányosítjuk az osztályt, másnéven objektumot hozunk létre az osztályból
+
+            TobbElemOsztaly a = new TobbElemOsztaly();
+
+            // Szám megadása
+            while (paros + paratlan < 100)
+            {
+                Console.WriteLine("Adj meg egy egész számot!");
+                szam = Convert.ToInt32(Console.ReadLine());
+
+                a.GetSzam(szam);
+                a.SetSzamTipus();
+                if(a.outSzamTipus())
+                {
+                    valasz = "páros";
+                    paros+=szam;
+                }
+                else
+                {
+                    valasz = "páratlan";
+                    paratlan+=szam;
+                }
+                Console.WriteLine($"A szám tipusa:{valasz}, összege:{paros+paratlan} ");
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
