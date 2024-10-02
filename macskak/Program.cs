0cfc6c907127992a476b66f9db2d2de69b0d4874
Misi47 <Csire07@gmail.com>
2024-10-02 08:08:46 +0200

    macska

---
+++ macskak/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace macskak
+{
+    class Program
+    {
+        enum animal {macska, kutya, tigris, farkas}
+        static void Main(string[] args)
+        {
+            animal b = animal.farkas;
+            animal c = animal.macska;
+
+            if (b==animal.farkas)
+            {
+                Console.WriteLine("A b állat farkas");
+            }
+            if (c == animal.macska)
+            {
+                Console.WriteLine("A c állat macska");
+            }
+        }
+    }
+}
