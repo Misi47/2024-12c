e3a650965190d51244d09bf2491ca9ba569eedd2
2021125 <csire07@gmail.com>
2024-10-02 09:53:30 +0200

    haág

---
+++ haág/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace haág
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b = true;
+            if (b) Console.WriteLine("Ez az igaz ág");
+            else Console.WriteLine("Ez a hamis ág");
+
+            bool a = false;
+            if (a) Console.WriteLine("Ez az igaz ág");
+            else Console.WriteLine("Ez a hamis ág");
+
+            Console.ReadKey();
+        }
+    }
+}
