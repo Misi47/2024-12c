ece307c0108e8b6982a520d7773a3e4abaa14c95
Misi47 <Csire07@gmail.com>
2024-10-02 07:13:18 +0200

    fgd

---
+++ Matrix/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ Projekt neve: Matrix
+ Készítsen osztályt az alábbiak szerint.
+ Projekt neve: MatrixOOP
+ Osztály neve: Matrix
+ Az osztály feladata:
+ Egy N x N-es mátrix elemeit rajzolja ” ki a konzol képernyőre, végül kiszámítja a determinánsát.
+ Metódusok:
+ - Konstruktor
+ - Mátrix elemeinek beállítása.
+ - Mátrix kiírása a konzolra
+ - Determináns meghatározása
+ - Determináns lekérése
+ */ 
+
+namespace Matrix
+{
+    class Matrix
+    {
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
